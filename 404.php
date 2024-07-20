ef4d8bebfd057185a6e4616cac47996c9cfe8fb6
Angelo Peralta <128838611+angelop-1602@users.noreply.github.com>
2024-07-20 16:03:19 +0800

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 50px;
+        }
+        h1 {
+            font-size: 36px;
+        }
+    </style>
+</head>
+<body>
+    <h1>404 - Not Found</h1>
+    <p>The page you are looking for might have been removed or its name changed.</p>
+</body>
+</html>
